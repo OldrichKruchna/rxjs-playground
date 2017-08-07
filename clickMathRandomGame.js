9aff55d0f9ca6da6bdead731e9979c59e4d5f10e
Oldrich Kruchna <kruchna.o@gmail.com>
2017-08-07 07:39:27 +0200

    add simple click Math.random() game with Rxjs Observables

---
+++ clickMathRandomGame.js
@@ -0,0 +1,21 @@
+const click = Rx.Observable.fromEvent(document, 'click');
+const rndVal = Rx.Observable.create(observer => {  
+    setInterval(() => observer.next(Math.random()), 1000);
+}).share();
+
+rndVal.takeUntil(click).subscribe(value => console.log(value));
+
+click.withLatestFrom(rndVal).subscribe((values) => {
+    const lastVal = values[1];
+
+    if (lastVal > 0.5) { 
+        console.log(`%cWin with value ${lastVal}`, 'color: green; font-size:15px;');
+    } else {
+        console.log(`%cLose with value ${lastVal}`, 'color: red; font-size:15px;');
+    }
+});
+
+
+
+
+
